e32bc78842c471e689b9d7cafcb6bad43e8dff9e
Jonás Onofre <jonasonofreduran@gmail.com>
2022-05-19 15:27:10 -0500

    Terminación todas las vistas. Falta Header y Menú

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+function circle() {
+
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+
+}
+
+document.getElementById('prba').addEventListener('click', circle);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
